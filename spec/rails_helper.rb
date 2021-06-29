24166ba0b09b545e44d25ac2783de3975c217780
Luiz Affonso <luizbuchhalter@outlook.com>
2021-06-29 01:59:49 -0300

    Update config to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,8 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 require 'spec_helper'
 require 'support/factory_bot'
-require 'database_cleaner'
+require "active_support"
+require "database_cleaner/active_record"
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
