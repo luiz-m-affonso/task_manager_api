2976f20685bc5f29049ad6ae8c72a78e56653946
Luiz Affonso <luizbuchhalter@outlook.com>
2021-07-12 22:22:29 -0300

    Add shoulda matchers for user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,38 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # subject { build(:user) }
-  # before { @user = FactoryBot.build(:user)}
   let(:user) { build(:user) }
 
   it { is_expected.to validate_presence_of(:name)}
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive}
+  it { is_expected.to validate_confirmation_of(:password)}
+  it { is_expected.to allow_value("luiz@gmail.com").for(:email)}
 
-  # it { expect(user).to respond_to(:email)}
+  it { expect(user).to respond_to(:email)}
 
-  # context 'when name is blank' do
-  #   before { user.name = '' }
+  context 'when name is blank' do
+    before { user.name = '' }
 
-  #   it { expect(user).not_to be_valid }
-  # end
+    it { expect(user).not_to be_valid }
+  end
 
-  # context 'when name is nil' do
-  #   before { user.name = nil }
+  context 'when name is nil' do
+    before { user.name = nil }
 
-  #   it { expect(user).not_to be_valid}
-  # end
-
-  # it { expect(subject).to respond_to(:email) }
-  # it { expect(subject).to respond_to(:password) }
-  # it { expect(subject).to respond_to(:password_confirmation) }
-  # it { expect(subject).to be_valid }
-
-  # it { is_expected.to respond_to(:email) }
-  # it { is_expected.to respond_to(:password)}
-  # it { is_expected.to respond_to(:password_confirmation)}
-  # it { is_expected.to be_valid }
-
-  # it { expect(@user).to be_valid}
-  # it { expect(@user).to respond_to(:email) }
-  # it { expect(@user).to respond_to(:password)}
-  # it { expect(@suer).to respond_to(:password_confirmation)}
+    it { expect(user).not_to be_valid}
+  end
 end
