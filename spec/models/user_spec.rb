766c9ea34522dda6268c8c30ce817dc16bd08854
Luiz Affonso <luizbuchhalter@outlook.com>
2021-07-12 22:22:54 -0300

    Refactoring User specs with shoulda matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,18 +7,4 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_uniqueness_of(:email).case_insensitive}
   it { is_expected.to validate_confirmation_of(:password)}
   it { is_expected.to allow_value("luiz@gmail.com").for(:email)}
-
-  it { expect(user).to respond_to(:email)}
-
-  context 'when name is blank' do
-    before { user.name = '' }
-
-    it { expect(user).not_to be_valid }
-  end
-
-  context 'when name is nil' do
-    before { user.name = nil }
-
-    it { expect(user).not_to be_valid}
-  end
 end
