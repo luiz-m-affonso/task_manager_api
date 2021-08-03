33b9cfbef99de772eb2490b39a9b7679dea6115c
Luiz Affonso <luizbuchhalter@outlook.com>
2021-08-03 00:22:48 -0300

    Adding delete and update to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   respond_to :json
+  before_action :set_user, only: %i[update destroy]
 
   def show
     begin
@@ -19,9 +20,28 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def update
+
+    if @user.update(user_params)
+      render json: @user, status: 200
+    else
+      render json: @user.errors, status: 422
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      render json: @user, status: 204
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
