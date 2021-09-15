e61b014376448bef059c39f89af3295328662038
Luiz Affonso <luizbuchhalter@outlook.com>
2021-09-14 22:01:05 -0300

    Add auth token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,17 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_confirmation_of(:password)}
   it { is_expected.to allow_value("luiz@gmail.com").for(:email)}
   it { is_expected.to validate_uniqueness_of(:auth_token)}
+
+  describe '#info?' do
+    it 'returns email and created_at' do
+
+    end
+  end
+
+  describe "#info" do
+    it 'returns email and created_at' do
+      user.save!
+      expect(user.info).to  eq("#{user.email} - #{user.created_at}")
+    end
+  end
 end
